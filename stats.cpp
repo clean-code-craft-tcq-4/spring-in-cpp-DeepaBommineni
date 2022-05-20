4f481ecd44ed2a79793d30de64c46330d9777720
Deepa Bommineni (MS/EDX3-XC) <Bommineni.Deepa@in.bosch.com>
2022-05-20 12:53:11 +0530

    Added impelentation in stats header and source files

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,26 @@
 #include "stats.h"
-
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+#include "algorithm"
+#include "numeric"
+namespace Statistics {
+Stats Statistics::ComputeStatistics(const std::vector<double>& numList) {
     //Implement statistics here
+	Stats statObj;
+	double maximum = NAN;
+	double minimum = NAN;
+    double sum = 0.0;
+
+	if(numList.size() != 0){
+	maximum = *max_element(numList.begin(), numList.end());
+	minimum = *min_element(numList.begin(), numList.end());
+	}
+	statObj.max = maximum;
+	statObj.min = minimum;
+	if (numList.size() != 0) {
+    for (const double &i: numList) {
+        sum += (double)i;
+    }
+	}
+    statObj.average = sum / numList.size();
+	return statObj;
 }
+} //namespace Statistics
--- stats.h
+++ stats.h
@@ -1,5 +1,43 @@
 #include <vector>
-
+#include "algorithm"
+#include "numeric"
 namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
-}
+    Stats ComputeStatistics(const std::vector<double>& numList);
+class Stats {
+	public:
+		double average, min, max;
+};
+} // namespace Statistics
+
+class IAlerter
+{};
+class EmailAlert:public IAlerter
+{
+	public:
+		EmailAlert():emailSent(false){}
+		bool emailSent;
+};
+class LEDAlert:public IAlerter
+{
+	public:
+		LEDAlert():ledGlows(false){}
+		bool ledGlows;
+};
+
+class StatsAlerter {
+	public:
+	    StatsAlerter(const float maxThreshold, std::vector<IAlerter*>& alerters):m_maxThreshold(maxThreshold), m_alerters(alerters){}
+
+		void checkAndAlert(std::vector<double> alerters) {
+	        double value = *max_element(alerters.begin(),alerters.end());
+			if (value > m_maxThreshold) {
+				emailAlert.emailSent = true;
+				ledAlert.ledGlows = true;
+			}
+	    }
+    public:
+		const float m_maxThreshold;
+		std::vector<IAlerter*> m_alerters;
+		EmailAlert emailAlert;
+		LEDAlert ledAlert;
+};
